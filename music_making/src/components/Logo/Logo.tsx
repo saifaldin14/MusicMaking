47a59b246e8cd73bd7ad6b3fa225ffb742205c70
saifaldin14 <saifaldindeveloper@gmail.com>
2021-04-13 18:53:49 -0400

    Create Logo.tsx

---
+++ music_making/src/components/Logo/Logo.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from "react";
+import "./style.css";
+
+const Logo: FunctionComponent = () => {
+  return (
+    <h1 className="logo">
+      <span role="img" aria-label="metal hand emoji">
+        ￿
+      </span>
+      <span role="img" aria-label="musical keyboard emoji">
+        ￿
+      </span>
+      <span role="img" aria-label="musical notes emoji">
+        ￿
+      </span>
+    </h1>
+  );
+};
+
+export default Logo;
