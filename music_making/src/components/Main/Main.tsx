5020a5b44c4de0b28dacaa193860bed6d78f8d22
saifaldin14 <24593380+saifaldin14@users.noreply.github.com>
2021-06-06 19:31:33 -0400

    Connected piano with keyboard and added event listeners and sound

--- music_making/src/components/Main/Main.tsx
+++ music_making/src/components/Main/Main.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent } from "react";
-import { Keyboard } from "../Keyboard/Keyboard";
-import { NoAudioMessage } from "../NoAudioMessage/NoAudioMessage";
-import { useAudioContext } from "../AudioContextProvider/useAudioContext";
+import { KeyboardWithInstrument } from "../Keyboard";
+import { NoAudioMessage } from "../NoAudioMessage";
+import { useAudioContext } from "../AudioContextProvider";
 
 export const Main: FunctionComponent = () => {
   const AudioContext = useAudioContext();
-  return !!AudioContext ? <Keyboard /> : <NoAudioMessage />;
+  return !!AudioContext ? <KeyboardWithInstrument /> : <NoAudioMessage />;
 };
