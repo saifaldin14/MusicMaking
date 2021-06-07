9236e83eefa30a9b7d7d2e67b44b6c89465bf38f
saifaldin14 <24593380+saifaldin14@users.noreply.github.com>
2021-06-07 19:19:32 -0400

    Added Instrument selector

--- music_making/src/state/Instrument/Provider.tsx
+++ music_making/src/state/Instrument/Provider.tsx
@@ -1,16 +1,13 @@
-import { createContext, useContext } from "react";
-import { InstrumentName } from "soundfont-player";
+import React, { FunctionComponent, useState } from "react";
 import { DEFAULT_INSTRUMENT } from "../../domain/sound";
+import { InstrumentContext } from "./Context";
 
-export interface ContextValue {
-  instrument: InstrumentName;
-  setInstrument: (instrument: InstrumentName) => void;
-}
+export const InstrumentContextProvider: FunctionComponent = ({ children }) => {
+  const [instrument, setInstrument] = useState(DEFAULT_INSTRUMENT);
 
-export const InstrumentContext = createContext<ContextValue>({
-  instrument: DEFAULT_INSTRUMENT,
-  setInstrument() {},
-});
-
-export const InstrumentContextConsumer = InstrumentContext.Consumer;
-export const useInstrument = () => useContext(InstrumentContext);
+  return (
+    <InstrumentContext.Provider value={{ instrument, setInstrument }}>
+      {children}
+    </InstrumentContext.Provider>
+  );
+};
