e6e7393236a3be24dbfb28fd3c126864aa6c55e1
saifaldin14 <24593380+saifaldin14@users.noreply.github.com>
2021-06-07 19:22:29 -0400

    Update InstrumentSelector.tsx

---
+++ music_making/src/components/InstrumentSelector/InstrumentSelector.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent, ChangeEvent } from "react";
+import { InstrumentName } from "soundfont-player";
+import { useInstrument } from "../../state/Instrument";
+import { options } from "./options";
+import "./style.css";
+
+const InstrumentSelector: FunctionComponent = () => {
+  const { instrument, setInstrument } = useInstrument();
+  const updateValue = ({ target }: ChangeEvent<HTMLSelectElement>) =>
+    setInstrument(target.value as InstrumentName);
+
+  return (
+    <select className="instruments" onChange={updateValue} value={instrument}>
+      {options.map(({ label, value }) => (
+        <option key={value} value={value}>
+          {label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default InstrumentSelector;
