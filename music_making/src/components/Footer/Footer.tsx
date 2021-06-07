218c5f12c483038eb3850adef6cad62ff2051273
saifaldin14 <saifaldindeveloper@gmail.com>
2021-06-07 19:55:28 -0400

    Update Footer.tsx

--- music_making/src/components/Footer/Footer.tsx
+++ music_making/src/components/Footer/Footer.tsx
@@ -1,14 +1,16 @@
-import React, { FunctionComponent } from "react"
-import "./style.css"
+import React, { FunctionComponent } from "react";
+import "./style.css";
 
 export const Footer: FunctionComponent = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="footer">
-      <a href="https://newline.co">Newline.co</a>
+      <a href="https://saifaldin.ca" target="_blank">
+        Saif Al-Din Ali
+      </a>
       <br />
       {currentYear}
     </footer>
-  )
-}
+  );
+};
