2e5e1c2f8c64f67087f9509e6dca08b1c79ebebf
saifaldin14 <24593380+saifaldin14@users.noreply.github.com>
2021-06-07 19:50:39 -0400

    Update

--- music_making/src/App.tsx
+++ music_making/src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Footer } from "./components/Footer/Footer";
-import { Logo } from "./components/Logo/Logo";
+import { Footer } from "./components/Footer";
+import { Logo } from "./components/Logo";
+import { Main } from "./components/Main";
 import "./App.css";
 
-const App = () => {
+export const App = () => {
   return (
     <div className="app">
       <Logo />
-      <main className="app-content" />
+      <main className="app-content">
+        <Main />
+      </main>
       <Footer />
     </div>
   );
 };
-
-export default App;
